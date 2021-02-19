2b4a9c0a0690048a126b250082029a547ae2b698
Slowo207 <54341345+Slowo207@users.noreply.github.com>
2021-02-19 15:32:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+print('pass 1')
+
 display = Display(visible=0, size=(800, 600))
 display.start()
 
+print('pass 2')
+
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
+
+print('pass 3')
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 driver.get('http://nytimes.com')
