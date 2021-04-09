a4a9e7232f38d1936d89eea2ec18d3e512e283a6
Slowo207 <54341345+Slowo207@users.noreply.github.com>
2021-04-09 15:50:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,17 @@ from selenium import webdriver
 display = Display(visible=0, size=(800, 600))
 display.start()
 
+print("test1)
+
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
+      
+print("test2")
 
 driver = webdriver.Chrome(options=options)
 driver.get('http://nytimes.com')
+      
+      print("test3")
+      
 print(driver.title)
+print("test4")
