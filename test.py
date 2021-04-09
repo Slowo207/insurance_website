c17f509860cf87c67318a2362b7fb9ff3a697a1e
Slowo207 <54341345+Slowo207@users.noreply.github.com>
2021-04-09 15:36:48 +0800

    Create test.py

--- test.py
+++ test.py
@@ -1,28 +1,12 @@
 from pyvirtualdisplay import Display
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-print('pass 1')
 
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-print('pass 2')
-
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
 
-print('pass 3')
-
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-print('pass 4.1')
+driver = webdriver.Chrome(chrome_options=options)
 driver.get('http://nytimes.com')
-print('pass 4.2')
 print(driver.title)
-
-print('pass 5')
-
-exit()
