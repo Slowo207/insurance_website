ada99eb95ce217fce9d18e91ad96fd6f15159f5b
Slowo207 <54341345+Slowo207@users.noreply.github.com>
2021-04-09 15:58:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-print("test1)
+print("test1")
 
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
